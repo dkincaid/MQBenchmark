4472c273ef9bd494492a45ab2e50d621041b0189
Dave Kincaid <kincaid.dave@gmail.com>
2011-12-13 20:20:17 -0600

    Added command line parameters to control which system is tested and how many messages are sent.

--- src/main/java/MQBenchmarkRunner.java
+++ src/main/java/MQBenchmarkRunner.java
@@ -7,20 +7,37 @@
 public class MQBenchmarkRunner {
     public static final String QUEUENAME = "vet2pet-test";
     public static final String RABBITHOST = "localhost";
-    public static final String SQSURI = "localhost";
+    public static final String SQSURI = "https://queue.amazonaws.com/028042250788/v2p_data_testqueue";
 
+    static int numberOfMessages = 10;
+    static String systemToCheck = "both";
+    
     public static void main(String[] argv) {
-        int numberOfMessages = 10;
+        if (argv.length == 2) {
+            if (argv[0].equals("rabbitmq"))
+                systemToCheck = "rabbitmq";
+            else if (argv[0].equals("sqs"))
+                systemToCheck = "sqs";
+            else if (argv[0].equals("both"))
+                systemToCheck = "both";
+            else
+                printUsage();
+        }
+       
 
-        //startRabbitMQPublisher(numberOfMessages);
-        //startRabbitMQConsumer(numberOfMessages);
+        if (systemToCheck.equals("rabbitmq") || systemToCheck.equals("both")) {
+            startRabbitMQPublisher(numberOfMessages);
+            startRabbitMQConsumer(numberOfMessages);
+        }
 
-        startSQSPublisher(numberOfMessages);
-        startSQSConsumer(numberOfMessages);
+        if (systemToCheck.equals("sqs") || systemToCheck.equals("both")) {
+            startSQSPublisher(numberOfMessages);
+            startSQSConsumer(numberOfMessages);
+        }
     }
 
     private static void startSQSPublisher(int numberOfMessages) {
-        createAndStartPublisherThread(new SQSMessagePublisher(RABBITHOST), numberOfMessages);
+        createAndStartPublisherThread(new SQSMessagePublisher(SQSURI), numberOfMessages);
     }
 
     private static void startRabbitMQPublisher(int numberOfMessages) {
@@ -36,13 +53,21 @@ public class MQBenchmarkRunner {
     private static void startSQSConsumer(int numberOfMessages) {
         createAndStartConsumerThread(new SQSMessageConsumer(SQSURI), numberOfMessages);
     }
+
     private static void startRabbitMQConsumer(int numberOfMessages) {
         createAndStartConsumerThread(new RabbitMQMessageConsumer(RABBITHOST, QUEUENAME), numberOfMessages);
     }
-
     private static void createAndStartConsumerThread(MessageConsumer messageConsumer, int numberOfMessages) {
         ConsumerRunner consumerRunner = new ConsumerRunner(messageConsumer, numberOfMessages);
         Thread consumerThread = new Thread(consumerRunner);
         consumerThread.start();
+    }
+
+    private static void printUsage() {
+        System.out.println("Usage: MQBenchmarkRunner <systemTOCheck> <numberOfMessages>");
+        System.out.println("");
+        System.out.println("where <systemToCheck> is rabbitmq, sqs or both.");
+        System.out.println("If no arguments are given the default is both and 10.");
+        System.exit(0);
     }
 }
